dd5ed29dbd80622921a07196caf3e91eb2592f01
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-11-20 21:36:37 +0300

    create test generator project

---
+++ TestsGenerator/TestsGenerator/TestClassInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace TestsGenerator
+{
+    public class TestClassInfo
+    {
+        public string TestClassName { get; set; }
+        public string TestClassCode { get; set; }
+
+        public TestClassInfo(string name, string code)
+        {
+            TestClassName = name;
+            TestClassCode = code;
+        }
+    }
+}
